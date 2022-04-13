1b51ff47db2f2c288bd94b0dde9b5df62000cdbc
lostoneway <abishop416@gmail.com>
2022-04-13 12:11:07 -0500

    Use setInterval to loop through array of drinks, placing items in the DOM

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM\
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
 //set up event listener on the click to run our getDrink function 
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink(){
   //grab drink from input
-  let drink = document.querySelector('input').value
-.toLowerCase()
+  let drink = document.querySelector('input').value.toLowerCase();
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.drinks)
-      //place drink name, thumbnail and instructions into DOM 
-      document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('p').innerText = data.drinks[0].strInstructions
-
-      //loop through array 
-      
+      console.log(data.drinks);
+      //make function to grab new drink on refresh  
+      refresh(data.drinks);
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
-
 }
-    
+
+function refresh(drinks){
+  let counter = 0 
+  let interval = setInterval(function(){
+    // console.log(drinks[counter].strDrink);
+    if(drinks[counter].strDrink !== null){
+      document.querySelector('h2').innerText = drinks[counter].strDrink;
+    }
+    // console.log(drinks[counter].strDrinkThumb);
+    if(drinks[counter].strDrink !== null){
+      document.querySelector('img').src = drinks[counter].strDrinkThumb;
+    }
+    // console.log(drinks[counter].strInstructions);
+    if(drinks[counter].strDrink !== null){
+      document.querySelector('p').innerText = drinks[counter].strInstructions;
+    }
+    // console.log(counter);
+    if(counter == drinks.length -1){
+      clearInterval(interval); 
+    }
+    counter++; 
+  },2500)
+}
